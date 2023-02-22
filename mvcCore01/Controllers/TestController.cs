92e52220766c096d79a54fe89156fd68f06ef723
Jorge Javier Izaguirre <jizaguirre@huenei.com>
2023-02-22 16:20:14 -0300

    Agregar archivos de proyecto.

---
+++ mvcCore01/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace mvcCore01.Controllers
+{
+    public class TestController : Controller //El nombre de la clase debe terminar con Controller. No así el nombre del archivo. No sé si la ubicación importa.
+    {
+        //
+        // GEt: /HelloWord/
+
+        //public string Index() // Método sin vistas
+        //{
+        //    return "This is my default action...";
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Temp()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWord/Welcome/
+
+        //public string Welcome() // Método sin parámetros.
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+
+        public string Welcome(string name, int ID = 1) //Método con parámetros.
+        //Funciona de esta manera
+        //https://localhost:7172/test/welcome?name=javier&ID=25
+        //Pero como en Program.cs está definido: pattern: "{controller=Home}/{action=Index}/{id?}");
+        //Tambien funciona:
+        //https://localhost:7172/test/welcome/25/?name=javier
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
---
+++ mvcCore01/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvcCore01.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        public string Adress{ get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
