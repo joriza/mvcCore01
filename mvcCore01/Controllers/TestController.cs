91973936bbc5d289a63f267cda92175efaba2131
Jorge Javier Izaguirre <jizaguirre@huenei.com>
2023-02-23 13:15:51 -0300

    230223-1315

--- mvcCore01/Controllers/TestController.cs
+++ mvcCore01/Controllers/TestController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using mvcCore01.Models;
 using System.Text.Encodings.Web;
 
 namespace mvcCore01.Controllers
@@ -31,14 +32,37 @@ namespace mvcCore01.Controllers
         //    return "This is the Welcome action method...";
         //}
 
-        public string Welcome(string name, int ID = 1) //Método con parámetros.
-        //Funciona de esta manera
-        //https://localhost:7172/test/welcome?name=javier&ID=25
-        //Pero como en Program.cs está definido: pattern: "{controller=Home}/{action=Index}/{id?}");
-        //Tambien funciona:
-        //https://localhost:7172/test/welcome/25/?name=javier
+        //public string Welcome(string name, int ID = 1) //Método con parámetros.
+        ////Funciona de esta manera
+        ////https://localhost:7172/test/welcome?name=javier&ID=25
+        ////Pero como en Program.cs está definido: pattern: "{controller=Home}/{action=Index}/{id?}");
+        ////Tambien funciona:
+        ////https://localhost:7172/test/welcome/25/?name=javier
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //}
+
+        public IActionResult Welcome(string name, int age = 18) 
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello" + name;
+            ViewData["Age"] = age;
+
+            return View();
+        }
+
+        public IActionResult Details(int? id) 
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = new Person();
+            person.Name = "John Wick";
+            person.PhoneNumber= "12345";
+            person.Email= "john@email.com";
+
+            return View(person);
         }
     }
 }
